3478145b84f903a59b1feee45832190751ec65c5
DaandinhoPy94 <daanvdster@gmail.com>
2025-09-23 18:14:10 +0200

    SEO opschonen pagina's

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ import LmArena from "./pages/LmArena";
 
 import Topic from "./pages/Topic";
 import TopicPage from "./pages/TopicPage";
-import TopicsRedirect from "./pages/TopicsRedirect";
 import RSSFeeds from "./pages/RSSFeeds";
 import Meer from "./pages/Meer";
 import Search from "./pages/Search";
@@ -37,32 +36,6 @@ import SitemapArticles from "./components/SitemapArticles";
 import SitemapTopics from "./components/SitemapTopics";
 import SitemapPapers from "./components/SitemapPapers";
 
-// Topic pages
-import TextToTextLlms from "./pages/topics/TextToTextLlms";
-import TextToImage from "./pages/topics/TextToImage";
-import TextToSpeechSpeechToText from "./pages/topics/TextToSpeechSpeechToText";
-import TextToVideo3D from "./pages/topics/TextToVideo3D";
-import RoboticsEmbodiedAi from "./pages/topics/RoboticsEmbodiedAi";
-import Gezondheidszorg from "./pages/topics/Gezondheidszorg";
-import CreatieveIndustrie from "./pages/topics/CreatieveIndustrie";
-import OnderwijsTraining from "./pages/topics/OnderwijsTraining";
-import FinancienBusiness from "./pages/topics/FinancienBusiness";
-import OverheidPubliekeSector from "./pages/topics/OverheidPubliekeSector";
-import AiOnderzoekslabs from "./pages/topics/AiOnderzoekslabs";
-import AcademischePublicaties from "./pages/topics/AcademischePublicaties";
-import Modeldoorbraken from "./pages/topics/Modeldoorbraken";
-import BigTech from "./pages/topics/BigTech";
-import StartupsScaleUps from "./pages/topics/StartupsScaleUps";
-import ChipmakersInfrastructuur from "./pages/topics/ChipmakersInfrastructuur";
-import VerenigdeStaten from "./pages/topics/VerenigdeStaten";
-import Europa from "./pages/topics/Europa";
-import Azie from "./pages/topics/Azie";
-import AiSafetyAlignment from "./pages/topics/AiSafetyAlignment";
-import EthiekBias from "./pages/topics/EthiekBias";
-import MilitairCyber from "./pages/topics/MilitairCyber";
-import ArbeidsmarktSkills from "./pages/topics/ArbeidsmarktSkills";
-import ProductiviteitAutomatisering from "./pages/topics/ProductiviteitAutomatisering";
-import PubliekDebatCultuur from "./pages/topics/PubliekDebatCultuur";
 import TopicsOverview from "./pages/TopicsOverview";
 
 const queryClient = new QueryClient();
@@ -105,32 +78,6 @@ const App = () => (
                 </ProtectedRoute>
               } />
               
-              {/* Topic pages */}
-              <Route path="/topics/text-to-text-llms" element={<TextToTextLlms />} />
-              <Route path="/topics/text-to-image" element={<TextToImage />} />
-              <Route path="/topics/text-to-speech-speech-to-text" element={<TextToSpeechSpeechToText />} />
-              <Route path="/topics/text-to-video-3d" element={<TextToVideo3D />} />
-              <Route path="/topics/robotics-embodied-ai" element={<RoboticsEmbodiedAi />} />
-              <Route path="/topics/gezondheidszorg" element={<Gezondheidszorg />} />
-              <Route path="/topics/financien-business" element={<FinancienBusiness />} />
-              <Route path="/topics/creatieve-industrie" element={<CreatieveIndustrie />} />
-              <Route path="/topics/onderwijs-training" element={<OnderwijsTraining />} />
-              <Route path="/topics/overheid-publieke-sector" element={<OverheidPubliekeSector />} />
-              <Route path="/topics/ai-onderzoekslabs" element={<AiOnderzoekslabs />} />
-              <Route path="/topics/academische-publicaties" element={<AcademischePublicaties />} />
-              <Route path="/topics/modeldoorbraken" element={<Modeldoorbraken />} />
-              <Route path="/topics/big-tech" element={<BigTech />} />
-              <Route path="/topics/startups-scale-ups" element={<StartupsScaleUps />} />
-              <Route path="/topics/chipmakers-infrastructuur" element={<ChipmakersInfrastructuur />} />
-              <Route path="/topics/verenigde-staten" element={<VerenigdeStaten />} />
-              <Route path="/topics/europa" element={<Europa />} />
-              <Route path="/topics/azie" element={<Azie />} />
-              <Route path="/topics/ai-safety-alignment" element={<AiSafetyAlignment />} />
-              <Route path="/topics/ethiek-bias" element={<EthiekBias />} />
-              <Route path="/topics/militair-cyber" element={<MilitairCyber />} />
-              <Route path="/topics/arbeidsmarkt-skills" element={<ArbeidsmarktSkills />} />
-              <Route path="/topics/productiviteit-automatisering" element={<ProductiviteitAutomatisering />} />
-              <Route path="/topics/publiek-debat-cultuur" element={<PubliekDebatCultuur />} />
               
               {/* Additional pages */}
               <Route path="/over-ons" element={<OverOns />} />
@@ -145,9 +92,6 @@ const App = () => (
               {/* New topic page route */}
               <Route path="/topic/:slug" element={<TopicPage />} />
               
-              {/* Redirect old topics routes */}
-              <Route path="/topics/:slug" element={<TopicsRedirect />} />
-              <Route path="/topics" element={<TopicsRedirect />} />
               
               <Route path="/:slug" element={<Topic />} />
               <Route path="/rss-feeds" element={<RSSFeeds />} />
