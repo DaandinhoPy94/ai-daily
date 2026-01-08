8f4fd5b35ea97a2b1fd861f61ed2d7e47299bf60
DaandinhoPy94 <daanvdster@gmail.com>
2026-01-08 22:03:34 +0100

    kleur aangpasssing toch iets fout gegaan

--- ai-daily-ios-swiftui/AIDagelijksNative/AIDagelijksNative/Extensions/Color+Extensions.swift
+++ ai-daily-ios-swiftui/AIDagelijksNative/AIDagelijksNative/Extensions/Color+Extensions.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 extension Color {
-    static let brandTeal = Color(red: 1.0, green: 0.34, blue: 0.13) // #FF5722 approx
     static let brandTeal = Color(red: 57.0/255.0, green: 109.0/255.0, blue: 118.0/255.0) // #396D76
     static let brandBackground = Color(uiColor: .systemGroupedBackground) // Off-white/Light Grey
 }
