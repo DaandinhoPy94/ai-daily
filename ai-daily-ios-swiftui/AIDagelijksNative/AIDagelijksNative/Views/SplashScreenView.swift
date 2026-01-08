1e7d296c3b2cc2db2316735b94987e3eb05836b9
DaandinhoPy94 <daanvdster@gmail.com>
2026-01-08 21:38:54 +0100

    logo voor darkmodus aangezet

--- ai-daily-ios-swiftui/AIDagelijksNative/AIDagelijksNative/Views/SplashScreenView.swift
+++ ai-daily-ios-swiftui/AIDagelijksNative/AIDagelijksNative/Views/SplashScreenView.swift
@@ -6,12 +6,16 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @Environment(\.colorScheme) private var colorScheme
+    
     var body: some View {
+        let imageName = colorScheme == .dark ? "splash-logo-dark" : "splash-logo"
+        
         ZStack {
             Color.brandBackground
                 .ignoresSafeArea()
 
-            Image("splash-logo")
+            Image(imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 150, height: 150)
